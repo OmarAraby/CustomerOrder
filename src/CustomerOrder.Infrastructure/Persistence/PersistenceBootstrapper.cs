da75101963706312dcbefa34f0e3f23dad7172ab
Omar Araby <omararaby789@gmail.com>
2026-08-19 22:38:43 +0300

    Refactor PersistenceBootstrapper for better organization
    Moved PersistenceBootstrapper to a new namespace
    `CustomerOrder.Infrastructure.Persistence` and added a
    `Configure` method to disable the database initializer for
    `AppDbContext`. Updated `using` directives to include
    `CustomerOrder.Infrastructure.Persistence.Context`.

--- src/CustomerOrder.Infrastructure/Persistence/PersistenceBootstrapper.cs
+++ src/CustomerOrder.Infrastructure/Persistence/PersistenceBootstrapper.cs
@@ -1,11 +1,12 @@
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using CustomerOrder.Infrastructure.Persistence.Context;
 
 namespace CustomerOrder.Infrastructure.Persistence
+{
 
     public static class PersistenceBootstrapper
     {
-  
+
         public static void Configure()
         {
             Database.SetInitializer<AppDbContext>(null);
